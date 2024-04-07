68510b054da493db3c29ea93724105034c4535b3
edomanuli <edomanuli@gmail.com>
2024-04-06 21:58:53 -0500

    ErrorDetails and ExceptionMiddlewareExtension

---
+++ TandemTechAPI/Extensions/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,32 @@
+﻿using Repository.Contracts;
+using Entities.ErrorModel;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace TandemTechAPI.Extensions
+{
+    public class ExceptionMiddlewareExtension
+    {
+        public static void ConfigureExceptionHandler(IApplicationBuilder app, ILoggerManager logger)
+        {
+            app.UseExceptionHandler(error =>
+            {
+                error.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        var errorDetails = new ErrorDetails
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error. Please try again later."
+                        };
+                        await context.Response.WriteAsync(errorDetails.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
