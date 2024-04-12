d9f2da3bdb1d6f425fc04b2fc09a65df8be76159
taf172 <trees172@gmail.com>
2024-04-12 10:06:33 -0400

    added chris/anuli registered users with seed data

--- Repository/Configuration/UserConfiguration.cs
+++ Repository/Configuration/UserConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -30,12 +31,38 @@ namespace Repository.Configuration
                    .IsRequired();
 
             //Seed Data
+            var hasher = new PasswordHasher<User>();
+
+            var anuli = new User
+            {
+                Id = 3,
+                UserName = "anuli",
+                NormalizedUserName = "ANULI",
+                Email = "anuli@example.com",
+                NormalizedEmail = "ANULI@EXAMPLE.COM",
+                FirstName = "Anuli",
+                LastName = "Edom"
+            };
+            anuli.PasswordHash = hasher.HashPassword(anuli, "12345");
+
+            var chris = new User
+            {
+                Id = 5,
+                UserName = "chris",
+                NormalizedUserName = "CHRIS",
+                Email = "CHRIS@example.com",
+                NormalizedEmail = "CHRIS@EXAMPLE.COM",
+                FirstName = "Chris",
+                LastName = "Leipold"
+            };
+            chris.PasswordHash = hasher.HashPassword(chris, "12345");
+
             builder.HasData(
-                new User { Id = 1, FirstName = "Emma", LastName = "Johnson", Email = "emma.johnson@example.com" },
-                new User { Id = 2, FirstName = "Liam", LastName = "Williams", Email = "liam.williams@example.com" },
-                new User { Id = 3, FirstName = "Olivia", LastName = "Brown", Email = "olivia.brown@example.com" },
+                anuli,
+                chris,
+                new User { Id = 1, FirstName = "Olivia", LastName = "Brown", Email = "olivia.brown@example.com" },
                 new User { Id = 4, FirstName = "Noah", LastName = "Jones", Email = "noah.jones@example.com" },
-                new User { Id = 5, FirstName = "Ava", LastName = "Garcia", Email = "ava.garcia@example.com" },
+                new User { Id = 2, FirstName = "Ava", LastName = "Garcia", Email = "ava.garcia@example.com" },
                 new User { Id = 6, FirstName = "Oliver", LastName = "Miller", Email = "oliver.miller@example.com" },
                 new User { Id = 7, FirstName = "Isabella", LastName = "Davis", Email = "isabella.davis@example.com" },
                 new User { Id = 8, FirstName = "Ethan", LastName = "Martinez", Email = "ethan.martinez@example.com" },
