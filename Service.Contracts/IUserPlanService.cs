0a628dd1787ee88820f98f634176c993eb0751eb
taf172 <trees172@gmail.com>
2024-04-15 13:01:04 -0400

    updated misnamed input param in UserPlan service interface

--- Service.Contracts/IUserPlanService.cs
+++ Service.Contracts/IUserPlanService.cs
@@ -5,7 +5,7 @@ namespace Service.Contracts
     public interface IUserPlanService
     {
         Task<IEnumerable<PlanInfoDto>> GetPlanOptions();
-        Task<IEnumerable<UserPlanDto>> GetUserPlans(int userPlanId);
+        Task<IEnumerable<UserPlanDto>> GetUserPlans(int userId);
         Task<UserPlanDto> EnrollUserInPlan(int userId, UserPlanCreateDto userPlanEnrollmentDto);
         Task CancleUserPlan(int userPlanId);
     }
