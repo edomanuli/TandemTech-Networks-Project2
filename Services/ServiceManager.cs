afb2d95f53f4535956596876dc160574e37896cb
taf172 <trees172@gmail.com>
2024-04-16 10:55:57 -0400

    renamed planinfo controller, info controller for device/plan info

--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -10,7 +10,7 @@ namespace Service
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IUserService> _userService;
-        private readonly Lazy<IPlanInfoService> _planInfoService;
+        private readonly Lazy<IInfoService> _infoService;
         private readonly Lazy<IUserPlanService> _userPlanService;
         private readonly Lazy<IAssignedNumberService> _assignedNumberService;
         private readonly Lazy<IPhoneNumberService> _phoneNumberService;
@@ -20,7 +20,7 @@ namespace Service
         public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
         {
             _userService = new Lazy<IUserService>(() => new UserService(repositoryManager, logger, mapper, userManager, configuration));
-            _planInfoService = new Lazy<IPlanInfoService>(() => new PlanInfoService(repositoryManager, logger, mapper));
+            _infoService = new Lazy<IInfoService>(() => new InfoService(repositoryManager, logger, mapper));
             _userPlanService = new Lazy<IUserPlanService>(() => new UserPlanService(repositoryManager, logger, mapper));
             _assignedNumberService = new Lazy<IAssignedNumberService>(() => new AssignedNumberService(repositoryManager, logger, mapper));
             _phoneNumberService = new Lazy<IPhoneNumberService>(() => new PhoneNumberService(repositoryManager, logger, mapper));
@@ -29,7 +29,7 @@ namespace Service
         }
 
         public IUserService User => _userService.Value;
-        public IPlanInfoService PlanInfo => _planInfoService.Value;
+        public IInfoService Info => _infoService.Value;
         public IUserPlanService UserPlan => _userPlanService.Value;
         public IAssignedNumberService AssignedNumber => _assignedNumberService.Value;
         public IPhoneNumberService PhoneNumber => _phoneNumberService.Value;
